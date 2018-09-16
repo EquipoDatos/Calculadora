74b5b7e3c8e7a5f1e340a40942b60eac36cc2c4f
marcopalermo <marcopalermo@192.168.0.7>
2018-09-16 12:36:41 -0500

    Soy Marco

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -17,6 +17,7 @@ public class Calculadora {
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("");
+        System.out.println("Soy Marco");
     }
     
 }
