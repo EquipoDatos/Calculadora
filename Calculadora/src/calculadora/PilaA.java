9501cbe784114883f05cfd013e7bc5ca4ab6e738
alberto <alberto@192.168.15.38>
2018-09-16 12:54:05 -0500

    Agregue clase de PilaA y EmptyCollectionException

---
+++ Calculadora/src/calculadora/PilaA.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author alberto
+ */
+
+
+public class PilaA<T> implements PilaADT<T> {
+    private T[] pila;
+    private int tope;
+    private final int MAX=20;
+    
+    public PilaA(){
+        pila=(T[])new Object[MAX];
+        tope=-1;
+    }
+    
+    public PilaA(int max){
+        pila=(T[])new Object[max];
+        tope=-1;
+    }
+    
+    @Override
+    public boolean isEmpty(){
+        return tope == -1;
+    }
+    
+    @Override
+    public void push(T dato){
+        if(tope == pila.length-1)
+            expand();
+        tope ++;
+        pila[tope]=dato;
+    }
+    
+
+    private void expand(){
+        T[] nuevo = (T[])new Object[pila.length*2];
+        for(int i=0; i<=tope; i++)
+            nuevo[i]=pila[i];
+        pila=nuevo;
+    }
+    
+
+    @Override
+    public T pop(){
+        if(isEmpty())
+            throw new EmptyCollectionException("Pila vacía");
+        //El else es de adorno porque la exception lo saca del método
+        else{
+            T resultado = pila[tope];
+            pila[tope] = null;
+            tope--;
+            return resultado;  
+        }  
+    }
+    
+    @Override
+    public T peek(){
+        if(isEmpty())
+            throw new EmptyCollectionException("Pila vacía");
+        //El else es de adorno porque la exception lo saca del método
+        else{
+            T resultado = pila[tope];
+            return resultado;  
+        }
+    }
+    
+    
+}
