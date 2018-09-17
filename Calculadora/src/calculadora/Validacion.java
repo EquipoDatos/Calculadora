6ebd41eee7dd633b06082facda5ecb08b34e9f05
alberto <alberto@192.168.15.38>
2018-09-17 12:33:33 -0500

    Terminando Starbucks

--- Calculadora/src/calculadora/Validacion.java
+++ Calculadora/src/calculadora/Validacion.java
@@ -19,15 +19,50 @@ public class Validacion {
         return respuesta;
     }
     
-     public static boolean esOperador(char e){
+    public static boolean esOperador(char e){
         boolean respuesta = false;
-        if ((e>='0' && e<='9') || (e=='.')) {
+        char[] set = {'+','-','*','/'};
+        int i=0;
+        while (i<set.length && e != set[i])
+            i++;
+        if (i != set.length)
             respuesta = true;
-        }
         return respuesta;
     }
     
-    public boolean validacionParentesis(String expresion){
+    public static boolean colindaParentesis(String expresion){
+            char a, b;
+            int i=1;
+            boolean resp = true;
+            while(i<expresion.length() && resp){
+                a=expresion.charAt(i-1);
+                b=expresion.charAt(i);
+                if(b == '(')
+                    if(esOperando(a) || a == ')' )
+                        resp = false;
+                if(a == ')')
+                    if(esOperando(b) || b == '(' )
+                        resp = false;
+                i++;
+            }
+            return resp;
+    }
+    
+    public static boolean parentesisVacio(String expresion){
+        boolean resp = true;
+        int i = 1;
+        char a, b;
+        while(resp && i<expresion.length()){ 
+            a = expresion.charAt(i-1);
+            b = expresion.charAt(i);
+            if(a=='(' && b==')') 
+                resp=false;
+            i++;
+        } 
+        return resp;
+    }
+    
+    public static boolean validaParentesis(String expresion){
         boolean resp = true;
         PilaA<Character> pila = new PilaA();
         int i, n;
@@ -45,21 +80,67 @@ public class Validacion {
                     }
             i++;
         }
-        return pila.isEmpty() && resp;
+        return pila.isEmpty() && resp && colindaParentesis(expresion) && parentesisVacio(expresion);
     }
     
-    public boolean validaOperadores(String expresion){
+    public static boolean validaOperadores(String expresion){
         boolean resp=true;
         int i=0;
         int n=expresion.length();
+        if(esOperador(expresion.charAt(i)))
+            resp=false;
         while(i<n && resp){
             if(esOperador(expresion.charAt(i)))
                 if(i == n-1 || esOperador(expresion.charAt(i+1)))
                     resp=false;
+            i++;
         }
         return resp;
     }
+    
+    public static boolean puntoEntreOperandos(String expresion){
+        char a, b, c;
+        int i=2;
+        boolean resp=true;
+        while(i<expresion.length() && resp){
+            a=expresion.charAt(i-2);
+            b=expresion.charAt(i-1);
+            c=expresion.charAt(i);
+            if(b == '.' && esOperador(a) && esOperador(c))
+                resp=false;
+            i++;
+        }
+        return resp;
+    }
+    
+    public static boolean validaDecimales(String expresion){
+        boolean resp=true;
+        int i=1;
+        boolean bandera= false;
+        int n=expresion.length();
+        if(expresion.charAt(0) == '.')
+            bandera = true;
+        while(i<n && resp){
+            if(esOperador(expresion.charAt(i)))
+                bandera = false;
+            if(bandera && expresion.charAt(i) == '.')
+                    resp = false;
+            else if(bandera == false && expresion.charAt(i) == '.')
+                bandera = true;
+             i++;
+            }
+        return resp && puntoEntreOperandos(expresion);
+    }
+    
+    
+    public static boolean valida(String expresion){
+        return validaParentesis(expresion) && validaDecimales(expresion) && validaOperadores(expresion);
+    }
+    
     public static void main(String[] args) {
-        
+        System.out.println(valida("(7+6)+9+.(.+)"));
+        /*
+        falta validar que no haya puros operadores o un punto entre paréntesis.
+        */
     }
 }
